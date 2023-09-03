a9ca7d23f9a28703274baf16953ad472e5f32d89
Luciano <luciano.rodriguez59@gmail.com>
2023-09-03 13:15:51 -0300

    .

--- src/routes/cotizations.routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import { CotizationController } from '../controllers/cotizations.controllers.js';
-const router = Router();
-router.get('/', CotizationController.getDolarCotization);
-export default router;
